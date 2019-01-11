a39374f0c7e1422db71973e5253f714ccee6b190
BrasilFullAddon <42700894+BrasilFullAddon@users.noreply.github.com>
2019-01-11 19:12:06 -0200

    Update youtube.py

--- Plugins/plugin.video.brasilfull/youtube.py
+++ Plugins/plugin.video.brasilfull/youtube.py
@@ -6,4 +6,4 @@ host = "5lTNCZXeaZ2L3Fmcv02bj5ibpJWZ0NXYw9yL6MHc0RHa"
 tam = len(host)
 basedem = host[::-1]
 MainBase = base64.b64decode(basedem)
-addon = xbmcaddon.Addon('plugin.video.NeXus')
+addon = xbmcaddon.Addon('plugin.video.brasilfull')
