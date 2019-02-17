6f9720c1d7153212b881a0b74c8348b415777f1f
BrasilFullAddon <42700894+BrasilFullAddon@users.noreply.github.com>
2019-02-17 19:23:24 -0300

    Atualização 2.9

--- Plugins/plugin.video.brasilfull/youtube.py
+++
@@ -1,9 +0,0 @@
-import xbmcaddon
-import base64
-
-
-host = "5lTNCZXeaZ2L3Fmcv02bj5ibpJWZ0NXYw9yL6MHc0RHa"
-tam = len(host)
-basedem = host[::-1]
-MainBase = base64.b64decode(basedem)
-addon = xbmcaddon.Addon('plugin.video.brasilfull')
